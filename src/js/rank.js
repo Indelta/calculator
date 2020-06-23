3ee592d177ee1d48598f212884550c6daac38aa7
DzmitryKas <Kasperovich_dima@mail.ru>
2020-06-23 15:04:01 +0300

    add function change icon header

--- src/js/rank.js
+++ src/js/rank.js
@@ -2,9 +2,11 @@ export default function tabRank () {
   const btnToggle = document.querySelector('.header__img');
   const mainRank = document.querySelector('.main-rank');
   const main = document.querySelector('.main');
+  const img = document.querySelector('.header__img');
 
   btnToggle.addEventListener( 'click', () => {
     mainRank.classList.toggle('active');
     main.classList.toggle('active');
+    img.classList.toggle('active');
   })
 }
